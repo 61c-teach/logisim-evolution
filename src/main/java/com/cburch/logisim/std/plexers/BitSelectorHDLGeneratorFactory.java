b311f8c5066cb3ca45a19e2aa38456467512be57
Ryan Lund <ryan.lund@berkeley.edu>
2020-11-05 14:04:11 -0800

    Fix BitSelector Verilog generation at the expense of killing VHDL support

--- src/main/java/com/cburch/logisim/std/plexers/BitSelectorHDLGeneratorFactory.java
+++ src/main/java/com/cburch/logisim/std/plexers/BitSelectorHDLGeneratorFactory.java
@@ -33,6 +33,8 @@ import com.cburch.logisim.fpga.designrulecheck.Netlist;
 import com.cburch.logisim.fpga.designrulecheck.NetlistComponent;
 import com.cburch.logisim.fpga.gui.FPGAReport;
 import com.cburch.logisim.fpga.hdlgenerator.AbstractHDLGeneratorFactory;
+import com.cburch.logisim.instance.StdAttr;
+
 import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -48,6 +50,26 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   private static final String ExtendedBitsStr = "NrOfExtendedBits";
   private static final int ExtendedBitsId = -4;
 
+  private int calculateSelectBits(int input_bits, int output_bits) {
+  int groups = (input_bits + output_bits - 1) / output_bits - 1;
+  int selectBits = 1;
+    if(groups >0) {
+      while (groups != 1) {
+        groups >>= 1;
+        selectBits++;
+      }
+    }
+    return selectBits;
+  }
+
+  private int calculateExtendedBits(int sel_bits, int output_bits) {
+    int nr_of_slices = 1;
+    for (int i = 0; i < sel_bits; i++) {
+      nr_of_slices <<= 1;
+    }
+    return nr_of_slices * output_bits;
+  }
+
   @Override
   public String getComponentStringIdentifier() {
     return "BITSELECTOR";
@@ -56,65 +78,52 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   @Override
   public SortedMap<String, Integer> GetInputList(Netlist TheNetlist, AttributeSet attrs) {
     SortedMap<String, Integer> Inputs = new TreeMap<String, Integer>();
-    Inputs.put("DataIn", InputBitsId);
-    Inputs.put("Sel", SelectBitsId);
+    int output_bits = attrs.getValue(BitSelector.GROUP_ATTR).getWidth();
+    int input_bits = attrs.getValue(StdAttr.WIDTH).getWidth();
+    int select_bits = calculateSelectBits(input_bits, output_bits);
+    Inputs.put("DataIn", input_bits);
+    Inputs.put("Sel", select_bits);
     return Inputs;
   }
 
   @Override
   public ArrayList<String> GetModuleFunctionality(
-      Netlist TheNetlist, AttributeSet attrs, FPGAReport Reporter, String HDLType) {
+    Netlist TheNetlist, AttributeSet attrs, FPGAReport Reporter, String HDLType) {
     ArrayList<String> Contents = new ArrayList<String>();
     int output_bits = attrs.getValue(BitSelector.GROUP_ATTR).getWidth();
+    int input_bits = attrs.getValue(StdAttr.WIDTH).getWidth();
+    int select_bits = calculateSelectBits(input_bits, output_bits);
+    int extended_bits = calculateExtendedBits(select_bits, output_bits);
     if (HDLType.equals(VHDL)) {
-      Contents.add(
-          "   s_extended_vector(("
-              + ExtendedBitsStr
-              + "-1) DOWNTO "
-              + InputBitsStr
-              + ") <= (OTHERS => '0');");
-      Contents.add("   s_extended_vector((" + InputBitsStr + "-1) DOWNTO 0) <= DataIn;");
-      if (output_bits > 1)
-        Contents.add(
-            "   DataOut <= s_extended_vector(((to_integer(unsigned(Sel))+1)*"
-                + OutputsBitsStr
-                + ")-1 DOWNTO to_integer(unsigned(Sel))*"
-                + OutputsBitsStr
-                + ");");
-      else Contents.add("   DataOut <= s_extended_vector(to_integer(unsigned(Sel)));");
+      Contents.add("");
     } else {
-      Contents.add(
-          "   assign s_extended_vector[" + ExtendedBitsStr + "-1:" + InputBitsStr + "] = 0;");
-      Contents.add("   assign s_extended_vector[" + InputBitsStr + "-1:0] = DataIn;");
       if (output_bits > 1) {
-        Contents.add("   wire[513:0] s_select_vector;");
-        Contents.add("   reg[" + OutputsBitsStr + "-1:0] s_selected_slice;");
-        Contents.add("   assign s_select_vector[513:" + ExtendedBitsStr + "] = 0;");
-        Contents.add("   assign s_select_vector[" + ExtendedBitsStr + "-1:0] = s_extended_vector;");
-        Contents.add("   assign DataOut = s_selected_slice;");
-        Contents.add("");
-        Contents.add("   always @(*)");
-        Contents.add("   begin");
-        Contents.add("      case (Sel)");
-        for (int i = 15; i > 0; i--) {
-          Contents.add(
-              "         "
-                  + i
-                  + " : s_selected_slice <= s_select_vector[("
-                  + Integer.toString(i + 1)
-                  + "*"
-                  + OutputsBitsStr
-                  + ")-1:"
-                  + i
-                  + "*"
-                  + OutputsBitsStr
-                  + "];");
+        Contents.add("   reg["+ (output_bits-1) +":0] temp;");
+        if (extended_bits > input_bits) {
+          Contents.add("   assign s_extended_vector[" + (extended_bits - 1) + ":" + input_bits + "] = 0;");
         }
-        Contents.add(
-            "         default : s_selected_slice <= s_select_vector[" + OutputsBitsStr + "-1:0];");
+        Contents.add("   assign s_extended_vector[" + (input_bits - 1) + ":0] = DataIn;");
+        Contents.add("   always @(*) begin");
+        Contents.add("      case (Sel)");
+        for (int i = 0; i < Math.pow(2, select_bits); i++) {
+          Contents.add("         " + select_bits + "'d" + i + ": temp = s_extended_vector[" + ((i + 1) * output_bits - 1) + ":" + (i * output_bits) + "];");
+        }
+        Contents.add("         default: temp = s_extended_vector[" + (output_bits - 1) + ":0];");
         Contents.add("      endcase");
         Contents.add("   end");
-      } else Contents.add("   assign DataOut = s_extended_vector[Sel];");
+        Contents.add("   assign DataOut = temp;");
+      } else {
+        Contents.add("   reg temp;");
+        Contents.add("   always @(*) begin");
+        Contents.add("      case (Sel)");
+        for (int i = 0; i < Math.pow(2, select_bits); i++) {
+          Contents.add("         " + select_bits + "'d" + i + ": temp = DataIn[" + i + "];");
+        }
+        Contents.add("         default: temp = DataIn[0];");
+        Contents.add("      endcase");
+        Contents.add("   end");
+        Contents.add("   assign DataOut = temp;");
+      }
     }
     return Contents;
   }
@@ -122,7 +131,7 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   @Override
   public SortedMap<String, Integer> GetOutputList(Netlist TheNetlist, AttributeSet attrs) {
     SortedMap<String, Integer> Outputs = new TreeMap<String, Integer>();
-    int output_bits = (attrs.getValue(BitSelector.GROUP_ATTR).getWidth() == 1) ? 1 : OutputsBitsId;
+    int output_bits = attrs.getValue(BitSelector.GROUP_ATTR).getWidth();
     Outputs.put("DataOut", output_bits);
     return Outputs;
   }
@@ -130,11 +139,6 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   @Override
   public SortedMap<Integer, String> GetParameterList(AttributeSet attrs) {
     SortedMap<Integer, String> Parameters = new TreeMap<Integer, String>();
-    int output_bits = attrs.getValue(BitSelector.GROUP_ATTR).getWidth();
-    Parameters.put(InputBitsId, InputBitsStr);
-    if (output_bits > 1) Parameters.put(OutputsBitsId, OutputsBitsStr);
-    Parameters.put(SelectBitsId, SelectBitsStr);
-    Parameters.put(ExtendedBitsId, ExtendedBitsStr);
     return Parameters;
   }
 
@@ -142,17 +146,6 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   public SortedMap<String, Integer> GetParameterMap(
       Netlist Nets, NetlistComponent ComponentInfo, FPGAReport Reporter) {
     SortedMap<String, Integer> ParameterMap = new TreeMap<String, Integer>();
-    int sel_bits = ComponentInfo.GetComponent().getEnd(2).getWidth().getWidth();
-    int input_bits = ComponentInfo.GetComponent().getEnd(1).getWidth().getWidth();
-    int output_bits = ComponentInfo.GetComponent().getEnd(0).getWidth().getWidth();
-    ParameterMap.put(InputBitsStr, input_bits);
-    ParameterMap.put(SelectBitsStr, sel_bits);
-    if (output_bits > 1) ParameterMap.put(OutputsBitsStr, output_bits);
-    int nr_of_slices = 1;
-    for (int i = 0; i < sel_bits; i++) {
-      nr_of_slices <<= 1;
-    }
-    ParameterMap.put(ExtendedBitsStr, nr_of_slices * output_bits + 1);
     return ParameterMap;
   }
 
@@ -176,12 +169,16 @@ public class BitSelectorHDLGeneratorFactory extends AbstractHDLGeneratorFactory 
   @Override
   public SortedMap<String, Integer> GetWireList(AttributeSet attrs, Netlist Nets) {
     SortedMap<String, Integer> Wires = new TreeMap<String, Integer>();
-    Wires.put("s_extended_vector", ExtendedBitsId);
+    int output_bits = attrs.getValue(BitSelector.GROUP_ATTR).getWidth();
+    int input_bits = attrs.getValue(StdAttr.WIDTH).getWidth();
+    int select_bits = calculateSelectBits(input_bits, output_bits);
+    int extended_bits = calculateExtendedBits(select_bits, output_bits);
+    Wires.put("s_extended_vector", extended_bits);
     return Wires;
   }
 
   @Override
   public boolean HDLTargetSupported(String HDLType, AttributeSet attrs) {
-    return true;
+    return HDLType.equals(VERILOG);
   }
 }
